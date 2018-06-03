d3e0516b6f13da52bb975c46495ae154be77b691
jdonaires <jmdonairesr@gmail.com>
2018-06-03 00:22:34 -0500

    correccion en DBAccess
    se corrige la contraseña de la conexion

--- DAL/DBAccess.php
+++ DAL/DBAccess.php
@@ -6,7 +6,7 @@ class DBAccess
  public function __construct()
  {
    try {
-     $this->conn = new PDO('mysql:host=localhost;dbname=prueba', 'root', 'admin123');
+     $this->conn = new PDO('mysql:host=localhost;dbname=prueba', 'root', 'admin');
    } catch (PDOException $e) {
      print "¡Error!: " . $e->getMessage() . "<br/>";
      die();
