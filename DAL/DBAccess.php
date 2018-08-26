a562464a10857e51058ebf23a3aed309ef2183b1
jdonaires <jmdonairesr@gmail.com>
2018-08-25 21:48:10 -0500

    CONTRASEÑA
    Esta es mi contraseña local

--- DAL/DBAccess.php
+++ DAL/DBAccess.php
@@ -6,7 +6,7 @@ class DBAccess
  public function __construct()
  {
    try {
-     $this->conn = new PDO('mysql:host=localhost;dbname=cuestionario', 'root', '123456');
+     $this->conn = new PDO('mysql:host=localhost;dbname=cuestionario', 'root', '');
    } catch (PDOException $e) {
      print "¡Error!: " . $e->getMessage() . "<br/>";
      die();
